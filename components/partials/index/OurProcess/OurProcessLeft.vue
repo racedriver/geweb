3af124ee3b8545a72476cec5d3d4ca0c0e8ad207
Lars Artmann <LartyHD@gmail.com>
2021-01-14 18:19:08 +0100

    Decoupled OurProcess

---
+++ components/partials/index/OurProcess/OurProcessLeft.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container margin-bottom-double position-relative padding-top-bottom-double">
+    <div class="c-step-decoration__left"></div>
+    <div
+      class="col lg-6 md-12 order-first flexh-align-center padding-top-bottom-double no-margin-bottom-lg flexh-space-between position-relative">
+      <div class="c-step">{{ entry.id.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false}) }}</div>
+      <img :class="entry.special ? '' : 'margin-right hidden-only-lg'" :src="entry.image" width="128"/>
+      <img v-if="!entry.special" :src="entry.image" class="c-arrow-right hidden-md" width="128"/>
+    </div>
+    <div class="col lg-7 md-12 alignself-center no-margin-bottom-lg2">
+      <h3>{{ entry.title }}</h3>
+      <div class="margin-bottom">{{ entry.text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OurProcessLeft',
+  props: {
+    entry: {
+      id: Number,
+      image: String,
+      title: String,
+      text: String,
+      special: Boolean,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ components/partials/index/OurProcess/OurProcessRight.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container margin-bottom-double position-relative z-index10">
+    <div
+      class="col lg-7 md-12 alignself-center no-margin-bottom-lg color-white"
+    >
+      <h3>{{ entry.title }}</h3>
+      <div class="margin-bottom">{{ entry.text }}</div>
+    </div>
+    <div
+      class="col lg-6 md-12 order-first flexh-align-center flexh-space-between padding-top-bottom-double no-margin-bottom-lg position-relative"
+    >
+      <div class="c-step">
+        {{
+          entry.id.toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false,
+          })
+        }}
+      </div>
+      <img :class="entry.special ? '' : 'margin-left hidden-only-lg'" :src="entry.image" width="128"/>
+      <img v-if="!entry.special" :src="entry.image" class="c-arrow-left hidden-md"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OurProcessRight',
+  props: {
+    entry: {
+      id: Number,
+      image: String,
+      title: String,
+      text: String,
+      special: Boolean,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
