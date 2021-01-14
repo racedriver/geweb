72b20d2ac0571a8869f53922218b07fe4b3275ad
Lars Artmann <LartyHD@gmail.com>
2021-01-14 17:20:23 +0100

    Decoupled WhatWeDo

--- components/partials/index/WhatWeDo.vue
+++ components/partials/index/WhatWeDo.vue
@@ -18,127 +18,7 @@
     <div class="container is-fullwidth padding-left padding-right">
       <div class="col lg-12 xs-no-padding-left-right">
         <div class="w-layout-grid c-grid-services">
-          <div class="c-grid-services__item round-border">
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              height="128"
-              src="images/cloud%20computing.png"
-            />
-            <h3>Cloud Software</h3>
-            <div class="margin-bottom">
-              We will almost always suggest to develop cloud first because
-              it's often the right choice. If you have specific needs for your
-              cloud provider and/or your security we will discuss these during
-              the setup phase.
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
-          <div
-            class="c-grid-services__item pull-down round-border"
-            data-w-id="26c3a647-0233-582b-5d9a-3ec60d35adbd"
-          >
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              src="images/ux%20design.png"
-              width="128"
-            />
-            <h3>User Experience & Design</h3>
-            <div class="margin-bottom">
-              User Experience Is Most Important Since It Directly Translates
-              To Customer Satisfaction And Customer Retention And We Know That
-              From Our Own Experience.
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
-          <div class="c-grid-services__item round-border">
-            <h3>Application Development</h3>
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              height="128"
-              src="images/application%20development.png"
-            />
-            <div class="margin-bottom">
-              We Will Handle The Entire Process. Just Lean Back And Let Us Do
-              The Magic!
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
-          <div class="c-grid-services__item round-border">
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              height="128"
-              src="images/software.jpeg"
-            />
-            <h3>Data Management</h3>
-            <div class="margin-bottom">
-              Skylines One Will Take Care Of Your Application Hosting,
-              Databases And File Storage So You Don't Have To.
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
-          <div class="c-grid-services__item pull-down round-border">
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              height="128"
-              src="images/mqdefault_6s.png"
-            />
-            <h3>Robotics and Machine Learning</h3>
-            <div class="margin-bottom">
-              We Are Experts In The Fields Of Robotics, Machine Learning and
-              Industrial Automation (Industry 4.0). Try Us!
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
-          <div class="c-grid-services__item round-border">
-            <img
-              alt=""
-              class="margin-bottom round-border"
-              height="128"
-              src="images/industries.jpeg"
-            />
-            <h3>Different industries</h3>
-            <div class="margin-bottom">
-              We Have Experience In Many Industries Including Automotive,
-              Transport, Real Estate, Robotics, Machine Learning, Cloud,
-              Industry 4.0, IoT And More. Ask Us For Ideas On Your Project.
-            </div>
-            <a
-              class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
-              href="#"
-            >
-              <div class="button-primary-text">Let&#x27;s talk</div>
-            </a>
-          </div>
+          <WhatWeDoEntry v-for="entry in entries" :entry="entry"/>
         </div>
       </div>
     </div>
@@ -147,10 +27,56 @@
 
 <script>
 export default {
-  name: "WhatWeDo"
+  name: 'WhatWeDo',
+  data() {
+    return {
+      entries: [
+        {
+          image: "/images/cloud%20computing.png",
+          title: "Cloud Software",
+          text: " We will almost always suggest to develop cloud first because it's\n" +
+            "              often the right choice. If you have specific needs for your cloud\n" +
+            "              provider and/or your security we will discuss these during the\n" +
+            "              setup phase.",
+        },
+        {
+          extraClass: "pull-down",
+          image: "/images/ux%20design.png",
+          title: "User Experience & Design",
+          text: " User Experience Is Most Important Since It Directly Translates To\n" +
+            "              Customer Satisfaction And Customer Retention And We Know That From\n" +
+            "              Our Own Experience.",
+        },
+        {
+          image: "/images/application%20development.png",
+          title: "Application Development",
+          text: "We Will Handle The Entire Process. Just Lean Back And Let Us Do\n" +
+            "              The Magic!",
+        },
+        {
+          image: "/images/software.jpeg",
+          title: "Data Management",
+          text: " Skylines One Will Take Care Of Your Application Hosting, Databases\n" +
+            "              And File Storage So You Don't Have To.",
+        },
+        {
+          extraClass: "pull-down",
+          image: "/images/mqdefault_6s.png",
+          title: "Robotics and Machine Learning",
+          text: "We Are Experts In The Fields Of Robotics, Machine Learning and\n" +
+            "              Industrial Automation (Industry 4.0). Try Us!",
+        },
+        {
+          image: "/images/industries.jpeg",
+          title: "Different industries",
+          text: "  We Have Experience In Many Industries Including Automotive,\n" +
+            "              Transport, Real Estate, Robotics, Machine Learning, Cloud,\n" +
+            "              Industry 4.0, IoT And More. Ask Us For Ideas On Your Project.",
+        },
+      ],
+    }
+  },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ components/partials/index/WhatWeDo/WhatWeDoEntry.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="'c-grid-services__item ' + entry.extraClass">
+    <img :src="entry.image" class="margin-bottom" height="128"/>
+    <h3>{{ entry.title }}</h3>
+    <div class="margin-bottom">
+      {{ entry.text }}
+    </div>
+    <a
+      :href="entry.link || 'contact-us'"
+      class="button-primary is-ghost is-small on-lg-margin-bottom w-inline-block"
+    >
+      <div class="button-primary-text">{{ entry.button || "Let's talk" }}</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WhatWeDoEntry',
+  props: {
+    entry: {
+      extraClass: String,
+      image: String,
+      title: String,
+      text: String,
+      link: String,
+      button: String,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
