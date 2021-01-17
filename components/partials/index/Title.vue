13e7e277f8b14f9bdd285730cebcacfdd7707512
Lars Artmann <LartyHD@gmail.com>
2021-01-17 15:35:42 +0100

    Fixed an styling bug in Title

--- components/partials/index/Title.vue
+++ components/partials/index/Title.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="c-shadowing-text is-white" style="color: #30c47e">
-    <slot></slot>
+  <div class="Title">
+    <div class="c-shadowing-text is-white" style="color: #30c47e">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Title"
+  name: 'Title',
 }
 </script>
 
 <style scoped>
-
+.Title {
+  margin-top: 48px;
+}
 </style>
