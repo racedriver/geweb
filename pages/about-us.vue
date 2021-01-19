9e11d5dfc453ebd3eebe3328af4f3940eadf7940
Lars Artmann <LartyHD@gmail.com>
2021-01-19 16:05:38 +0100

    Improved About us styling (+picture)

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -3,43 +3,64 @@
     <div class="section aboutus margin-top background">
       <div class="container">
         <div class="col lg-12 text-align-center">
-          <h1 style="margin-bottom: 0;">About Us</h1>
+          <h1 style="margin-bottom: 0">About Us</h1>
         </div>
       </div>
     </div>
     <div class="section no-padding-bottom">
       <div class="container flexh-align-center">
-        <div class="col lg-6 xs-12 color-white">
+        <div class="color-white">
           <div class="pre-title color-white">.Love the hustle</div>
           <h2 class="margin-bottom">Our Company</h2>
-          <div class="w-richtext">
-            <p><b>Skylines One is a high-tech, cloud-first software company focusing on cutting edge technologies.
-              Our focus is user experience (UX/UI), Kotlin and cloud-first with Google Cloud and Firebase.
-              We always strive for excellence and only the Sky(lines One) is the limit!
-            </b></p>
-            <p>We are a team of ex-Apple engineers who want to make technology available to everyone.
-              “You gotta place the user experience first and then reverse engineer the technology from there”
-              This quote from Steve Jobs has made a great impact on the way we think about and engineer our software.
-              Working alongside Steve, the master of user experience, has undoubtedly prepared us to take on your engineering challenge.
-            </p>
-            <p>Our CTO, 17-year-old Lars Artmann, is an accomplished engineer and author of his book on software engineering with Kotlin.
-                With a high-level certification in Java, which is not typical for anyone his age, he quickly found that Kotlin has many
-                advantages over Java and other programming languages.
-            </p>
-            <p>
-              Since serverless applications and cloud-first development is the cutting edge of technology, we have decided that the best
-              use case is usually achieved with Google Cloud and Firebase. We are a Google Cloud Solutions provider and will be happy to
-              guide you through deployment on Google Cloud, AWS or Azure.
-            </p>
-            <p>
-              Our Team consists of people at all levels of life and career, but we have one thing in common: We will never accept that 100%
-              customer satisfaction is impossible. Because it is possible. We invite you to try us out: book an exploration call with us today!
-              <br></p>
+          <!--          <div class="w-richtext col-12">-->
+          <p>
+            <b>
+              Skylines One is a high-tech, cloud-first software company focusing
+              on cutting edge technologies. Our focus is user experience
+              (UX/UI), Kotlin and cloud-first with Google Cloud and Firebase. We
+              always strive for excellence and only the Sky(lines One) is the
+              limit!
+            </b>
+          </p>
+          <div style="  box-shadow: 10px 8px 6px -6px black;">
+            <img
+              alt="Skylines One - About Us"
+              src="../static/images/design-teamwork.jpg"
+              style="max-width: 40%; float: right; margin: 20px"
+            />
           </div>
-        </div>
-        <div class="col lg-1"></div>
-        <div class="col lg-6 xs-12 no-margin-bottom"><img alt="Skylines One - About Us" class="round-border"
-                                                          src="../static/images/aboutus.jpg?text=Skylines One">
+          <p>
+            We are a team of ex-Apple engineers who want to make technology
+            available to everyone. “You gotta place the user experience first
+            and then reverse engineer the technology from there” This quote from
+            Steve Jobs has made a great impact on the way we think about and
+            engineer our software. Working alongside Steve, the master of user
+            experience, has undoubtedly prepared us to take on your engineering
+            challenge.
+          </p>
+
+          <p>
+            Our CTO, 17-year-old Lars Artmann, is an accomplished engineer and
+            author of his book on software engineering with Kotlin. With a
+            high-level certification in Java, which is not typical for anyone
+            his age, he quickly found that Kotlin has many advantages over Java
+            and other programming languages.
+          </p>
+
+          <p>
+            Since serverless applications and cloud-first development is the
+            cutting edge of technology, we have decided that the best use case
+            is usually achieved with Google Cloud and Firebase. We are a Google
+            Cloud Solutions provider and will be happy to guide you through
+            deployment on Google Cloud, AWS or Azure.
+          </p>
+          <p>
+            Our Team consists of people at all levels of life and career, but we
+            have one thing in common: We will never accept that 100% customer
+            satisfaction is impossible. Because it is possible. We invite you to
+            try us out: book an exploration call with us today!
+            <br/>
+          </p>
         </div>
       </div>
     </div>
@@ -55,33 +76,50 @@
         <div class="col">
           <h2>Management Team</h2>
 
-          <div class="color-white"><b>Where innovation meets experience</b></div>
-
+          <div class="color-white">
+            <b>Where innovation meets experience</b>
+          </div>
         </div>
       </div>
       <div class="container">
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="Alex Mittler" class="margin-right round-border" src="../static/images/alex-mittler.jpg"
-                 width="300">
+            <img
+              alt="Alex Mittler"
+              class="margin-right round-border"
+              src="../static/images/alex-mittler.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Alex<br>Mittler</div>
+              <div class="c-team1__name">Alex<br/>Mittler</div>
               <div class="size-h4 margin-bottom-quad">CEO</div>
               <div class="c-team1__social">
-                <a class="fa-brand margin-right-small" href="https://www.linkedin.com/in/alexmittler/"></a>
+                <a
+                  class="fa-brand margin-right-small"
+                  href="https://www.linkedin.com/in/alexmittler/"
+                ></a
+                >
               </div>
             </div>
           </div>
         </div>
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="Lars Artmann" class="margin-right round-border" src="../static/images/lars-artmann.jpg"
-                 width="300">
+            <img
+              alt="Lars Artmann"
+              class="margin-right round-border"
+              src="../static/images/lars-artmann.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Lars<br>Artmann</div>
+              <div class="c-team1__name">Lars<br/>Artmann</div>
               <div class="size-h4 margin-bottom-quad">CTO</div>
               <div class="c-team1__social">
-                <a class="fa-brand margin-right-small" href="https://www.linkedin.com/in/larsartmann/"></a>
+                <a
+                  class="fa-brand margin-right-small"
+                  href="https://www.linkedin.com/in/larsartmann/"
+                ></a
+                >
               </div>
             </div>
           </div>
@@ -90,26 +128,44 @@
       <div class="container">
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="Alexandra Obendorfer" class="margin-right round-border"
-                 src="../static/images/alexa-obendorfer.jpg" width="300">
+            <img
+              alt="Alexandra Obendorfer"
+              class="margin-right round-border"
+              src="../static/images/alexa-obendorfer.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Alexandra<br>Obendorfer</div>
+              <div class="c-team1__name">Alexandra<br/>Obendorfer</div>
               <div class="size-h4 margin-bottom-quad">Operations Manager</div>
               <div class="c-team1__social">
-                <a class="fa-brand margin-right-small"
-                   href="https://www.linkedin.com/in/alexandra-obendorfer-963522120/"></a>
+                <a
+                  class="fa-brand margin-right-small"
+                  href="https://www.linkedin.com/in/alexandra-obendorfer-963522120/"
+                ></a
+                >
               </div>
             </div>
           </div>
         </div>
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="" class="margin-right round-border" src="../static/images/ron-sumay.jpg" width="300">
+            <img
+              alt=""
+              class="margin-right round-border"
+              src="../static/images/ron-sumay.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Ron<br>Sumay</div>
-              <div class="size-h4 margin-bottom-quad">Regional Sales Manager</div>
+              <div class="c-team1__name">Ron<br/>Sumay</div>
+              <div class="size-h4 margin-bottom-quad">
+                Regional Sales Manager
+              </div>
               <div class="c-team1__social">
-                <a class="fa-brand margin-right-small" href="https://www.linkedin.com/in/roniel-sumay-73b3401ba/"></a>
+                <a
+                  class="fa-brand margin-right-small"
+                  href="https://www.linkedin.com/in/roniel-sumay-73b3401ba/"
+                ></a
+                >
               </div>
             </div>
           </div>
@@ -118,24 +174,31 @@
       <div class="container flex-horizontal">
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="Amy Ross" class="margin-right round-border" src="../static/images/amy-ross.jpg" width="300">
+            <img
+              alt="Amy Ross"
+              class="margin-right round-border"
+              src="../static/images/amy-ross.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Amy<br>Ross</div>
+              <div class="c-team1__name">Amy<br/>Ross</div>
               <div class="size-h4 margin-bottom-quad">Lead Engineer</div>
-              <div class="c-team1__social">
-              </div>
+              <div class="c-team1__social"></div>
             </div>
           </div>
         </div>
         <div class="col lg-5 md-12">
           <div class="flex-horizontal position-relative">
-            <img alt="Malek Anderson" class="margin-right round-border" src="../static/images/malek-anderson.jpg"
-                 width="300">
+            <img
+              alt="Malek Anderson"
+              class="margin-right round-border"
+              src="../static/images/malek-anderson.jpg"
+              width="300"
+            />
             <div class="c-team1__info">
-              <div class="c-team1__name">Malek<br>Anderson</div>
+              <div class="c-team1__name">Malek<br/>Anderson</div>
               <div class="size-h4 margin-bottom-quad">Backend Engineer</div>
-              <div class="c-team1__social">
-              </div>
+              <div class="c-team1__social"></div>
             </div>
           </div>
         </div>
@@ -151,12 +214,12 @@
       </div>
 
       <div class="container">
-        <div class="col text-align-center lg-3 ">
-          <div class="heading-alpha is-plus margin-bottom ">12</div>
+        <div class="col text-align-center lg-3">
+          <div class="heading-alpha is-plus margin-bottom">12</div>
           <div class="is-bold">Custom Projects</div>
         </div>
-        <div class="col text-align-center lg-3 ">
-          <div class="heading-alpha is-plus margin-bottom ">39</div>
+        <div class="col text-align-center lg-3">
+          <div class="heading-alpha is-plus margin-bottom">39</div>
           <div class="is-bold">Digital Products</div>
         </div>
         <div class="col text-align-center lg-3">
@@ -164,7 +227,7 @@
           <div class="is-bold">Cups of coffee</div>
         </div>
         <div class="col text-align-center lg-3">
-          <div class="heading-alpha is-plus margin-bottom ">98%</div>
+          <div class="heading-alpha is-plus margin-bottom">98%</div>
           <div class="is-bold">Customer satisfaction</div>
         </div>
       </div>
@@ -173,7 +236,8 @@
       <div class="container">
         <div class="col lg-12 text-align-center">
           <h2>Interested in joining our team?</h2>
-          <div class="color-white margin-bottom-double">Come join our team at Skylines One and make awesome digital
+          <div class="color-white margin-bottom-double">
+            Come join our team at Skylines One and make awesome digital
             products!
           </div>
           <a class="button-primary w-inline-block" href="contact-us.html">
@@ -187,10 +251,8 @@
 
 <script>
 export default {
-  name: "AboutUs"
+  name: 'AboutUs',
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
