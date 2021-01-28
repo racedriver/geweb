0d4499d4222cbd5a791c65372901cdc836c58be5
Lars Artmann <LartyHD@gmail.com>
2021-01-28 21:04:01 +0100

    Fixed Magazine in index.vue

--- pages/magazine/index.vue
+++ pages/magazine/index.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="section position-relative overflow-hidden">
-    <Title>Magazine</Title>
-    <div class="container">
-      <div class="col lg-7 block-centered md-12 text-align-center color-white">
-        <h2>All articles</h2>
-      </div>
-    </div>
-    <div class="container is-wrapping">
-      <PostPreview v-for="entry in articles" :entry="entry"/>
-    </div>
-  </div>
+  <Magazine :articles="articles" />
 </template>
 
 <script>
