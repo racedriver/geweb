613988291d5a9cd7e5b40028a965644723c46f46
Lars Artmann <LartyHD@gmail.com>
2021-02-05 10:24:44 +0100

    Updated texts of WhatWeDo.vue

--- components/partials/index/WhatWeDo.vue
+++ components/partials/index/WhatWeDo.vue
@@ -49,64 +49,11 @@
 </template>
 
 <script>
+import data from "/static/data/what-we-do"
 export default {
   name: 'WhatWeDo',
   data() {
-    return {
-      keyword: 'DEPLOY FASTER',
-      title: 'Everything you need to bring your project into reality',
-      text:
-        'Out of your expectations, we create even better solutions. By pushing the boundaries we create truly innovative software applications.',
-      entries: [
-        {
-          image: '/images/cloud%20computing.png',
-          title: 'Cloud Software',
-          text:
-            " We will almost always suggest to develop cloud first because it's\n" +
-            '              often the right choice. If you have specific needs for your cloud\n' +
-            '              provider and/or your security we will discuss these during the\n' +
-            '              setup phase.',
-        },
-        {
-          image: '/images/ux%20design.png',
-          title: 'User Experience & Design',
-          text:
-            ' User Experience Is Most Important Since It Directly Translates To\n' +
-            '              Customer Satisfaction And Customer Retention And We Know That From\n' +
-            '              Our Own Experience.',
-        },
-        {
-          image: '/images/application%20development.png',
-          title: 'Application Development',
-          text:
-            'We Will Handle The Entire Process. Just Lean Back And Let Us Do\n' +
-            '              The Magic!',
-        },
-        {
-          image: '/images/software.jpeg',
-          title: 'Data Management',
-          text:
-            ' Skylines One Will Take Care Of Your Application Hosting, Databases\n' +
-            "              And File Storage So You Don't Have To.",
-        },
-        {
-          extraClass: 'wrapper-21to9',
-          image: '/images/mqdefault_6s.png',
-          title: 'Robotics and Machine Learning',
-          text:
-            'We Are Experts In The Fields Of Robotics, Machine Learning and\n' +
-            '              Industrial Automation (Industry 4.0). Try Us!',
-        },
-        {
-          image: '/images/industries.jpeg',
-          title: 'Different industries',
-          text:
-            '  We Have Experience In Many Industries Including Automotive,\n' +
-            '              Transport, Real Estate, Robotics, Machine Learning, Cloud,\n' +
-            '              Industry 4.0, IoT And More. Ask Us For Ideas On Your Project.',
-        },
-      ],
-    }
+    return data
   },
 }
 </script>
---
+++ static/data/what-we-do.js
@@ -0,0 +1,56 @@
+export default {
+  keyword: 'DEPLOY FASTER',
+  title: 'Everything you need to bring your project into reality',
+  text:
+    'Out of your expectations, we create even better solutions. By pushing the boundaries we create truly innovative software applications.',
+  entries: [
+    {
+      image: '/images/cloud%20computing.png',
+      title: 'Cloud Software',
+      text:
+        "We will almost always suggest to develop cloud first because it's the safest place for your data and also scalable" +
+        'that makes it often the right choice.\n ' +
+        'If you have specific needs for your cloud' +
+        "like provider/security/technology don't be shy, send us a message and we are happy to help.",
+    },
+    {
+      image: '/images/ux%20design.png',
+      title: 'User Experience & Design',
+      text:
+        'User experience is most important since it directly translates to\n' +
+        'customer satisfaction and customer retention and we know that from\n' +
+        'our own experience.',
+    },
+    {
+      image: '/images/application%20development.png',
+      title: 'Application Development',
+      text:
+        'We will handle the entire process. ' +
+        'Just lean back and let us do the magic!',
+    },
+    {
+      image: '/images/software.jpeg',
+      title: 'Data Management',
+      text:
+        "All the time sucking work that's needed to come up with managing data, will not longer be a worry for you. " +
+        "Don't fell to bad our ex-Apple geeks love the problem solving.",
+    },
+    {
+      image: '/images/mqdefault_6s.png',
+      title: 'Robotics',
+      text:
+        'You have a Robot? ' +
+        'Perfect we would love to play with real world objects. ' +
+        'Already an idea what you want to do with it? ' +
+        'Send us a message.',
+    },
+    {
+      image: '/images/industries.jpeg',
+      title: 'Different industries',
+      text:
+        'We love working with B2B from all industries since we can deliver the best value here. ' +
+        'Executing on user experience and software. ' +
+        'If you explain your business to us, we can deliver the best software for your business. May I ask, which sector are you in?',
+    },
+  ],
+}
