b5e1dc52b5d2365275a4bc499e396211e09273f5
Lars Artmann <LartyHD@gmail.com>
2021-02-05 04:04:52 +0100

    Updated styling of Partners/Focusefforts

--- pages/partners/focusefforts.vue
+++ pages/partners/focusefforts.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <HeadText title="Partners / Focusefforts"/>
-    <div class="max-w-6xl flex my-8 mx-auto h-64">
-      <img class="ml-auto" src="/images/partners/focus-efforts.png"/>
-      <svg class="text-red-600 h-40 self-center mr-12" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
-      </svg>
-      <img class="mx-auto" src="/images/logo%20v4.1%20with%20white%20circle%20arrows%20only%2072ppi.png"/>
-    </div>
-    <div class="max-w-3xl mx-auto">
+    <HeadText title="Partners / Focusefforts" />
+    <div class="max-w-6xl mx-auto">
+      <div class="flex my-8 h-64">
+        <img
+          class="mx-auto"
+          src="/images/logo%20v4.1%20with%20white%20circle%20arrows%20only%2072ppi.png"
+        />
+        <svg
+          class="text-red-600 h-40 self-center ml-12"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+        <img class="mx-auto" src="/images/partners/focus-efforts.png" />
+      </div>
       <iframe
-        class="w-full bg-green-300 rounded-t"
+        class="w-full bg-green-300 rounded-t-md"
         scrolling="no"
         src="https://docs.google.com/forms/d/e/1FAIpQLSeEGLKAULrzQqZhpz7mK_nN9o7WDVSmN-AxP5YxpZtdHgdmWg/viewform?embedded=true"
         frameborder="0"
