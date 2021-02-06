d0ea93b0ad55bd8ae61ee69172f92ac78d9c8dc1
Lars Artmann <LartyHD@gmail.com>
2021-02-06 14:52:01 +0100

    Removed "All articles" from Magazine

--- components/Magazine.vue
+++ components/Magazine.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="section position-relative overflow-hidden">
-    <Title>Magazine</Title>
-    <div class="container">
-      <div class="col lg-7 block-centered md-12 text-align-center">
-        <h2 class="text-white">All articles</h2>
-      </div>
-    </div>
+    <Title class="mb-12">Magazine</Title>
+<!--    <div class="container">-->
+<!--      <div class="col lg-7 block-centered md-12 text-align-center">-->
+<!--        <h2 class="text-white">All articles</h2>-->
+<!--      </div>-->
+<!--    </div>-->
     <div class="container is-wrapping">
       <PostPreview v-for="(entry, i) in articles" :entry="entry" :key="i" />
     </div>
@@ -20,5 +20,3 @@ export default {
   },
 }
 </script>
-
-<style scoped></style>
