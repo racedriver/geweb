174e2331742286a3a5d154f8ed407da7cbec8257
Lars Artmann <LartyHD@gmail.com>
2021-02-06 17:23:10 +0100

    Disabled image background in HeaderDesktop

--- components/partials/header/HeaderDesktop.vue
+++ components/partials/header/HeaderDesktop.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="max-w-7xl mx-auto w-full flex items-center relative">
-    <div class="absolute inset-0" v-if="this.$route.path !== '/'">
-      <img
-        class="h-full w-full object-cover"
-        src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
-        alt="People working on laptops"
-      />
-      <div
-        class="absolute inset-0 bg-green-700"
-        style="mix-blend-mode: multiply"
-      ></div>
-    </div>
+<!--    <div class="absolute inset-0" v-if="this.$route.path !== '/'">-->
+<!--      <img-->
+<!--        class="h-full w-full object-cover"-->
+<!--        src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"-->
+<!--        alt="People working on laptops"-->
+<!--      />-->
+<!--      <div-->
+<!--        class="absolute inset-0 bg-green-700"-->
+<!--        style="mix-blend-mode: multiply"-->
+<!--      ></div>-->
+<!--    </div>-->
     <a class="relative my-8 ml-8 lg:ml-16" href="/">
       <img
         class="h-32"
