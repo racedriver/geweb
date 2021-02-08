0f038af924052d6b68a6afde4956f828ade2b6c9
Lars Artmann <LartyHD@gmail.com>
2021-02-08 17:43:22 +0100

    Added data for portfolio

---
+++ static/data/portfolio.js
@@ -0,0 +1,37 @@
+export default {
+  entries: [
+    // {
+    //   title: 'Payroll Automation',
+    //   description1: 'Concept',
+    //   description2: 'Realisation',
+    //   image: '/images/payroll.jpg',
+    //   alt: 'Payroll Automation Software using Discord',
+    //   link: '',
+    // },
+    {
+      title: 'To-Do App',
+      description1: 'Concept',
+      description2: 'Development',
+      image: '/images/to-do.jpg',
+      alt: '"To-Do App',
+      link: '',
+    },
+    {
+      title: 'Self-Service Systems Integrations',
+      description1: 'Concept',
+      description2: 'Realisation',
+      image: '/images/systems.jpg',
+      alt: 'Self-Service Systems Integrations',
+      link: '',
+    },
+
+    {
+      title: 'VR/AR',
+      description1: 'Design',
+      description2: 'Development',
+      image: '/images/vr-ar.jpg',
+      alt: 'Virtual Reality, Augmented Reality',
+      link: '',
+    },
+  ],
+}
