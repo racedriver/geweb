6f610e89fe8e176468e8238cb8c50b5d1a4a08dc
Lars Artmann <LartyHD@gmail.com>
2021-02-08 09:59:17 +0100

    Fixed a url bug and add a config in Magazine

--- components/Magazine.vue
+++ components/Magazine.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="grid lg:grid-cols-3 gap-10 mx-8 sm:gap-12 sm:mx-12 xl:mx-16 xl:max-w-7xl xl:mx-auto items-stretch">
       <div class="grid-cols-1 pop-out-default" v-for="(entry, i) in articles" :entry="entry" :key="i" >
-        <a :href="/magazine/ + entry.slug" class="inline-block w-full wrapper-16to9">
+        <a :href="url(entry)" class="inline-block w-full wrapper-16to9">
           <img :src="entry.thumbnail"/>
         </a>
         <!--    <div class="c-gridpost__category">-->
         <!--      <a href="#">Design</a> / <a href="#">Technology</a>-->
         <!--    </div>-->
         <div class="dark-box h-full">
-          <a :href="url" class="c-gridpost__title-2 inline-block max-w-full">
+          <a :href="url(entry)" class="text-gray-700 inline-block">
             <h3 class="text-white mb-4">{{ entry.title }}</h3>
           </a>
           <div class="text-gray-400 mb-4">
             {{ entry.short }}
           </div>
-          <a :href="/magazine/ + entry.slug" class="text-green-500 font-normal">
-            Read more
+          <a :href="url(entry)" class="text-green-500 font-normal">
+            <DynamicText :value="readMore" />
           </a>
         </div>
       </div>
@@ -23,10 +23,17 @@
 </template>
 
 <script>
+import data from "/static/data/magazine"
 export default {
   name: 'Magazine',
   props: {
     articles: Array,
   },
+  data() {
+    return data
+  },
+  methods: {
+    url: (entry) => /magazine/ + entry.slug
+  }
 }
 </script>
