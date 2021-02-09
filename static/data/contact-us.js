fd5dbde0ced093608c1a52d3659968cee504d87a
Lars Artmann <LartyHD@gmail.com>
2021-02-09 08:57:27 +0100

    Development...

---
+++ static/data/contact-us.js
@@ -0,0 +1,9 @@
+export default {
+  copy: {
+    text: [
+      { value: 'Find out how to bring your business into the' },
+      { value: '20ies', class: 'text-green-500' },
+      { value: 'today' },
+    ],
+  },
+}
