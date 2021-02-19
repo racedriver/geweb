ceb0e1f77537902a313898cda979b051f6a26c9a
Lars Artmann <LartyHD@gmail.com>
2021-02-19 22:37:50 +0100

    Improved styling on the success page

--- pages/success.vue
+++ pages/success.vue
@@ -2,12 +2,12 @@
   <main class="py-16 pb-24">
     <div style="background-color: rgba(48, 196, 126, 0.95)">
       <div class="max-w-7xl mx-auto lg:grid grid-cols-3 gap-8 relative">
-        <div class="col-span-2 col-start-2 p-20 mb-1 text-white">
+        <div class="col-span-2 col-start-2 p-20 mb-1 text-white text-xl">
           <h1>Thank you!</h1>
           <h2>Your submission has been received!</h2>
         </div>
         <div
-          class="col-span-1 lg:absolute lg:top-1/4 lg:w-96 pt-20 pb-4 px-16 bg-white shadow-2xl rounded-md text-black"
+          class="col-span-1 lg:absolute lg:top-1/4 lg:w-96 pt-12 pb-4 px-12 bg-white shadow-2xl rounded-md text-black"
         >
           <div class="text-lg mb-6">
             Lewes, DA 19958
