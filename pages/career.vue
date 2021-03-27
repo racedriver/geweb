8f47f3e3452d8511b7747f0f45094e2f796bc6b2
Wlad <wlad@skylines.one>
2021-03-27 22:16:28 +0100

    Updating Career.vue with functioning and color adjusted videoask workflow.

--- pages/career.vue
+++ pages/career.vue
@@ -1,42 +1,36 @@
 <template>
-  <div class="text-white">
-    <HeadText title="Careers"/>
-    <div class="section no-padding-bottom">
-      <div class="z-10 flex flex-wrap lg:flex-nowrap w-full max-w-7xl mx-auto items-center">
-        <div class="col lg-6 xs-12 text-white">
-          <div class="pre-title text-white">.Love the hustle</div>
-          <h2 class="mb-6">Our Company</h2>
-          <div class="w-richtext">
-            <p><b>Skylines One is a high-tech, cloud-first software company focusing on cutting edge
-              technologies. Moving the needle that bit further for our clients. The company’s name resembles its
-              tradition of flying high in the sky, using only the very best lines of code - Skylines One</b></p>
-            <p>We are a bunch of ex-Apple engineers with a combined 30+ years experience in software architecture
-              and technology.<br></p>
-          </div>
-        </div>
-        <div class="col lg-1"></div>
-        <!--style="background: linear-gradient(to right,rgba(57, 128, 148, .90) 0%,  rgb(7, 48, 70) 25%, rgba(7, 48, 70,1) 75%, rgba(57, 128, 148, .90) 100%);"-->
-        <div class="col lg-6 xs-12 no-margin-bottom"><img alt="Careers at Skylines One" class="round-border"
-                                                          src="/images/career.jpg">
-        </div>
-      </div>
-    </div>
+  <section class="md:h-5/6 ">
+    <div class="
+         grid md:grid-flow-col md:w-10/12 md:mx-auto max-w-7xl
+         py-16 px-12 text-gray-50 sm:rounded bg--dark"
+         style="cursor:url(https://cdn.custom-cursor.com/db/4250/32/arrow2460.png),url(https://cdn.custom-cursor.com/db/7248/32/starter-3d-effect-cursor-cursor.png),
+        default!important">
 
-    <div class="section">
-      <div class="z-10 flex flex-wrap lg:flex-nowrap w-full max-w-7xl mx-auto">
-        <div class="col lg-12 text-align-center">
-          <h2>Interested in joining our team?</h2>
-          <div class="text-white mb-8 lg:mb-12">Come join our team at Skylines One and make awesome digital
-            products!
-          </div>
-          <a class="button-primary inline-block max-w-full" href="contact-us">
-            <div class="button-primary-text">Join Us</div>
-          </a>
+      <div class="grid-cols-1 md:w-2/3 place-content-center  self-center items-center">
+
+        <h1 class="place-content-center self-center items-center text-green-800 font-bold font text-6xl">
+          Career
+        </h1>
+
+        <div class="flex items-center text-2xl">
+          <p><br>Skylines One is a high-tech, cloud-first software company focusing on cutting edge
+            technologies. Moving the needle that bit further for our clients. The company’s name resembles its
+            tradition of flying high in the sky, using only the very best lines of code - Skylines One</br>
+            <br>We are a bunch of ex-Apple engineers with a combined 30+ years experience in software architecture
+            and technology.</br><br> Dont wait, join us right now!</br></p>
         </div>
       </div>
+
+
+      <iframe class="rounded-lg" src="https://www.videoask.com/fu2q1ls1t"
+              allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *;"
+              width="100%"
+              height="600px"
+      />
+
     </div>
-  </div>
-</template>
+  </section>
+  </template>
 
 <script>
 export default {
@@ -47,3 +41,6 @@ export default {
 <style scoped>
 
 </style>
+
+
+
