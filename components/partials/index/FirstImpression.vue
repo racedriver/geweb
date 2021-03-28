7b0286d60636435de867b6ef7ff47d318cde0d4e
racedriver <alex@skylines.one>
2021-03-28 09:11:56 +0200

    -add explainer video to main page -temporarily disable videoask

--- components/partials/index/FirstImpression.vue
+++ components/partials/index/FirstImpression.vue
@@ -1,49 +1,49 @@
 <template>
-  <section class="md:h-5/6 ">
-    <div class="
-         grid md:grid-flow-col md:w-10/12 md:mx-auto max-w-7xl
-         py-16 px-12 text-gray-50 sm:rounded bg--dark"
-         style="cursor:url(https://cdn.custom-cursor.com/db/4250/32/arrow2460.png),url(https://cdn.custom-cursor.com/db/7248/32/starter-3d-effect-cursor-cursor.png),
-        default!important">
-      <div class="grid-cols-1 md:w-2/3 place-content-center  self-center items-center">
+  <div class="mx-auto max-w-7xl h-5/6 sm:flex sm:flex-wrap sm:place-items-center" >
+    <div class="py-16 px-12 w-full text-gray-50 sm:rounded md:w-7/12 lg:w-5/12 xl:w-2/5 lg:ml-16 bg--dark"
+         style="cursor:url(https://cdn.custom-cursor.com/db/4250/32/arrow2460.png),url(https://cdn.custom-cursor.com/db/7248/32/starter-3d-effect-cursor-cursor.png), default!important">
+      <h1>
+        <div class="text-4xl ">{{ $t('FirstImpression.PreTitle') }}</div>
+        <div class="text-6xl bg-gradient-primary text-gradient pb-1 ">{{
+            $t('FirstImpression.Ex')
+          }}<span class="text-white opacity-75">{{ $t('FirstImpression.Split') }}</span
+        >{{ $t('FirstImpression.AppleEngineers') }}
+        </div>
+        <div class="text-2xl ">{{ $t('FirstImpression.SubTitle') }}</div>
+        <div class="mb-8 text-lg font-normal text-gray-300 ">
+          {{ $t('FirstImpression.SubSubTitle') }}
+        </div>
 
-        <h1 class="place-content-center self-center items-center">
-          <div class="text-4xl ">{{ $t('FirstImpression.PreTitle') }}</div>
-          <div class="text-6xl bg-gradient-primary text-gradient pb-1 ">{{
-              $t('FirstImpression.Ex')
-            }}<span class="text-white opacity-75">{{ $t('FirstImpression.Split') }}</span
-            >{{ $t('FirstImpression.AppleEngineers') }}
-          </div>
-          <div class="text-2xl ">{{ $t('FirstImpression.SubTitle') }}</div>
-          <div class="mb-8 text-lg font-normal text-gray-300 ">
-            {{ $t('FirstImpression.SubSubTitle') }}
-          </div>
-        </h1>
+      </h1>
 
-        <div class="flex items-center">
-          <nuxt-link
-            class="group py-8 w-full text-center text-white bg-green-500 rounded
+      <div class="flex items-center">
+        <nuxt-link
+          class="group py-8 w-full text-center text-white bg-green-500 rounded
             outline-white hover:no-underline hover:bg-black
            "
-            to="/contact-us"
-            style="cursor:url(https://cdn.custom-cursor.com/db/pointer/32/Kick_Me_Pointer.png) , pointer!important"
-          >
+          to="/contact-us"
+          style="cursor:url(https://cdn.custom-cursor.com/db/pointer/32/Kick_Me_Pointer.png) , pointer!important"
+        >
           <span class="text-lg sm:text-xl md:text-2xl text-gray-50 font-bold group-hover:text-gradient-secondary">
           {{ $t('FirstImpression.Button') }}
           </span>
-          </nuxt-link>
-        </div>
+        </nuxt-link>
       </div>
 
-      <div class="grid-cols-1 aspect-h-16 aspect-w-10 mt-12 md:mt-0 md:-ml-10">
-        <iframe class="rounded-lg" src="https://www.videoask.com/f641hztym"
-                allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *;"
-                width="100%"
-                height="100%"
-        />
-      </div>
     </div>
-  </section>
+    <!--div class="py-16 px-12 w-full text-gray-50 sm:rounded md:w-7/12 lg:w-5/12 xl:w-2/5 lg:ml-16 bg--dark">
+    <iframe src="https://www.videoask.com/f641hztym"
+            allow="camera *; microphone *; autoplay *; encrypted-media *; fullscreen *; display-capture *;"
+            width="100%"
+            height="60%"
+            style="border: none; border-radius: 24px"
+    >
+    </iframe>
+    </div-->
+
+
+    <div class="w-full xl:h-8"/>
+  </div>
 
 </template>
 
