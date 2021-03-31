6dc76ec07712db4b411e946492b9558347d12ca4
Wlad <wlad@skylines.one>
2021-03-31 23:26:18 +0200

    Updated portfolio with flex cards, links in cards head, plus media query to change the position of portfolio items from horizontal to vertical in mobile view.

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,39 +1,107 @@
 <template>
-  <div class="text-white">
-    <HeadText title="Portfolio" />
-    <main class="grid my-20 w-full max-w-7xl md:grid-cols-2 lg:grid-cols-3 gap-8 px-4-dynamic mx-auto">
-      <nuxt-link
-        class="inline-block relative bg-transparent no-underline aspect-w-16 aspect-h-9 "
-        :to="entry.link"
-        v-for="(entry, i) in this.entries"
-        :key="i"
-      >
-        <div class="group hover:bg-black text-black flex justify-center rounded">
-          <div
-            class=" group-hover:shadow-lg rounded
-             bg-cover
-             group-hover:bg-black group-hover:filter-brightness-25 absolute inset-0"
-            :style="{ backgroundImage: `url('${entry.image}')` }"
-          >
-          </div>
-          <p class="self-center flex text-3xl group-hover:text-white group-hover:z-10 text-center">
-            {{ entry.description1 }} - {{ entry.description2 }}
-          </p>
-        </div>
-      </nuxt-link>
-    </main>
+  <section>
+    <div class="text-white">
+      <HeadText title="Portfolio" />
+      <main class="grid my-20 w-full max-w-7xl md:grid-cols-2 lg:grid-cols-3 gap-8 px-4-dynamic mx-auto">
+        <nuxt-link
+          class="inline-block relative bg-transparent no-underline aspect-w-16 aspect-h-9 "
+          :to="entry.link"
+          v-for="(entry, i) in this.entries"
+          :key="i">
+        </nuxt-link>
+      </main>
+    </div>
+  <div class="container">
+    <div class="card">
+      <img src="../static/images/systems.jpg" width="50%" height="50%">
+      <div class="card__head"><img src="../static/images/logo v4.1 with black circle arrows only 72ppi.png" style="float:left;width:42px;height:42px;"><a href="https://www.iovent.net/">See iovent website.</a></div>
+    </div>
+    <div class="card">
+      <img src="../static/images/chronow.png">
+      <div class="card__head"><img src="../static/images/logo v4.1 with black circle arrows only 72ppi.png" style="float:left;width:42px;height:42px;"><a href="https://chronow.us/">See chronow website.</a></div>
+    </div>
+    <div class="card">
+      <img src="../static/images/myfitmentwebfit.png">
+      <div class="card__head"><img src="../static/images/logo v4.1 with black circle arrows only 72ppi.png" style="float:left;width:42px;height:42px;"><a href="https://play.google.com/store/apps/details?id=one.skylines.myfitment.app">Download app.</a></div>
+    </div>
   </div>
+  </section>
 </template>
 
 <script>
-import data from '/static/data'
 export default {
-  name: 'portfolio',
-  head: {
-    title: 'Portfolio'
-  },
-  data() {
-    return data.portfolio
-  },
+name: "myportfolio.vue"
 }
 </script>
+
+<style>
+
+body {
+  background: #000;
+  font-family: "Corbel Light";
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 10vmin;
+  overflow: hidden;
+  transform: skew(0deg); /*<!-- If deg set it will make rectangular shape -->*/
+.card {
+  flex: 1;
+  transition: all 1s ease-in-out;
+  height: 75vmin; /* This is the height of actual box itself */
+  position: relative;
+  margin-bottom: 10vmin;
+  margin-left: 5vmin;
+.card__head {
+  color: black;
+  background:  rgba(48, 196, 126, 0.5);
+  padding: 0.5em;
+  transform: rotate(-90deg);
+  transform-origin: 0% 0%;
+  transition: all 0.5s ease-in-out;
+  min-width: 15%; /* This is the width of card head, changing it to 15% from 100% heps specially in column view */
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  font-size: 1em;
+  white-space: nowrap;
+}
+
+&:hover {
+   flex-grow: 10;
+img {
+  filter: grayscale(0);
+}
+.card__head {
+  text-align: center;
+  top: calc(100% - 2em);
+  color: white;
+  background: rgba(0, 0, 0, 0.5);
+  font-size: 1.8em;
+  transform: rotate(0deg) skew(0deg);
+}
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: all 1s ease-in-out;
+  filter: grayscale(100%);
+}
+&:not(:nth-child(5)) {
+   margin-right: 1em;
+ }
+}
+}
+/* On screens that are 600px wide or less, make the portfolio items on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .container {
+    flex-direction: column;
+  }
+}
+
+</style>
+
